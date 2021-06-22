4238a2f22cf2b9ac4fbbf3ba0ec15425ea2eb89f
Lucas Bethuel <bethuel.pg@gmail.com>
2021-06-22 15:08:47 -0300

    clpp

---
+++ loginho/script.js
@@ -0,0 +1,46 @@
+const init = () => {
+    const inputUsuario = document.querySelector('input[type="usuario"]');
+    const inputPassword = document.querySelector('input[type="password"]');
+    const submitButton = document.querySelector('.login_submit');
+
+
+    const errorHandler = () => {
+        submitButton.classList.remove('success')
+        submitButton.classList.add('error');
+        submitButton.textContent = "Erro"
+    }
+
+    const successHandler = () => {
+        submitButton.classList.remove('error');
+        submitButton.classList.add('success');
+        submitButton.textContent = "Entrando";
+        window.location.href = "../home.html";
+    }
+
+    if (submitButton) {
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+
+
+            console.log(inputPassword.value, inputUsuario.value)
+
+            fetch('https://reqres.in/api/login', {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    email: inputUsuario.value,
+                    password: inputPassword.value
+                })
+            }).then((response) => {
+                if (response.status !== 200) {
+                    return errorHandler();
+                }
+                successHandler();
+            }).catch(() => {
+                errorHandler();
+            })
+        })
+    }
+}
+
+window.onload = init;
