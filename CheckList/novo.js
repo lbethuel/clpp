18b8cd3f34d8f83091ccc2e6d00e46073b6fde60
Lucas Bethuel <bethuel.pg@gmail.com>
2021-06-28 16:19:52 -0300

    edição 2

--- CheckList/novo.js
+++ CheckList/novo.js
@@ -2,13 +2,17 @@ function novaCheckList(){
     document.getElementById('novoCheckList').disabled = false;
 }
 
-const query = (a)=>document.querySelector(a);
 
-
-
-const add = query('.buttonSalvar');
-const oi = query('.perguntas')
+const add = document.querySelector('.buttonSalvar');
+const oi = document.querySelector('.perguntas')
+let copia = document.querySelector('article')
 
 add.addEventListener('click', () => {
     oi.setAttribute('class', 'pergunta0');
 })
+
+
+$( ".adicao1" ).click(function() {
+    let a= copia.cloneNode(true);
+    oi.appendChild(a);
+  });
