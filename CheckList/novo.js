052e8c7af70b9a041a2c202a8d8033400bd4b1ec
Lucas Bethuel <bethuel.pg@gmail.com>
2021-07-08 15:28:49 -0300

    edicao10

--- CheckList/novo.js
+++ CheckList/novo.js
@@ -11,8 +11,27 @@ add.addEventListener('click', () => {
     oi.setAttribute('class', 'pergunta0');
 })
 
-
 $( ".adicao1" ).click(function() {
-    let a= copia.cloneNode(true);
+    let a = copia.cloneNode(true);
     oi.appendChild(a);
+  });
+
+
+
+////////////////////////////////////////////////////////////
+ const ad2 = document.querySelector('.adicao3');
+  const adi = document.querySelector('.corpoPergunta1');
+
+/*  ad2.addEventListener('click', () => {
+    adi.setAttribute('class', 'corpoPergunta2');
+})  */ 
+
+/////////////////////////////////////////////////////////////
+let replicar = document.querySelector('.corpoPergunta1')
+const ad3 = document.querySelector('.perg');
+
+$( ".adicao3" ).click(function() {
+    let b = adi.cloneNode(true);
+    ad3.appendChild(b);
+    b.setAttribute('class', 'corpoPergunta2');
   });
